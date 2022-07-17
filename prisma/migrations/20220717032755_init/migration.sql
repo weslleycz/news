9cf8f4352339192df443a1bc7796594345e83658
Francisco Weslley <weslleysouza22@gmail.com>
2022-07-17 00:38:00 -0300

    Configurando o banco de dados

--- prisma/migrations/20220717032755_init/migration.sql
+++ prisma/migrations/20220717032755_init/migration.sql
@@ -1,19 +1,14 @@
-/*
-  Warnings:
-
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "User";
-
 -- CreateTable
 CREATE TABLE "Users" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
+    "stripeId" TEXT,
 
     CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_stripeId_key" ON "Users"("stripeId");
---
+++ src/.env.local.ts
@@ -0,0 +1,12 @@
+//Stripe
+export const STRIPE_API_KEY =
+  "sk_test_51LKTjwEZqcAoXWoe3qMXjLBMqCHAOE696wVNM8sVpVbnA00gfCR4S7fTpLKOSjP8VOlcrYLpn1OyW4kkFqfalnOL00n2hXno6Y";
+export const STRIPE_PUBLIC_KEY =
+  "pk_test_51LKTjwEZqcAoXWoeHWLAIiRgzU84d22pDHIGFLBjYEBx2BBCQ07ITYpNUqTN84mAkUk5G5kaQxxeWwp7eTD4Poef00YzWrlMmk";
+
+//Github
+export const GITHUB_ID = "e301e56709480f104598";
+export const GITHUB_SECRET = "45496b5c4771ff6de897df3b5430cbe7d9ba9674";
+
+//Local
+export const HOST = "http://localhost:3000";
